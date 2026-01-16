1f4188ec87f834c72d889323b4e66977db948fba
La Anh Quan <laanhquan1807@gmail.com>
2026-01-16 11:32:37 +0700

    update Chat message

--- src/main/java/vn/hoidanit/jobhunter/util/constant/AccessAction.java
+++ src/main/java/vn/hoidanit/jobhunter/util/constant/AccessAction.java
@@ -1,7 +1,8 @@
 package vn.hoidanit.jobhunter.util.constant;
 
 public enum AccessAction {
-    CREATE_JOB,   // HR đăng bài
-    APPLY_JOB,    // USER apply job
-    CHAT_AI
+    CREATE_JOB, // HR đăng bài
+    APPLY_JOB, // USER apply job
+    CHAT_AI,
+    CHAT_MESSAGES
 }
